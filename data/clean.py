80d8ee370a25347de757405f9b70baad08b5765c
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-04-20 21:03:06 +0530

    Upd: cleaning clean.py file

--- data/clean.py
+++ data/clean.py
@@ -6,27 +6,35 @@ x = df['Sentence']
 
 
 
-def cleanLines(lines):
+# def cleanLines(lines):
+#     clean_lines = []
+#     for line in lines:
+#         if len(line.split()) < 5: continue
+
+#         nline = re.sub('http[s]:\/\/\S+', '', line)
+#         nline = re.sub('@\S+', '', nline)
+#         nline = re.sub('\n', '', nline)
+#         nline = nline.strip()
+#         nline = nline.lower()
+#         nline = re.sub('-', ' ', nline)
+#         nline = re.sub('\'s', '', nline)
+#         nline = re.sub('\'', '', nline)
+#         nline = re.sub('\"', '', nline)
+#         nline = re.sub('[^a-zA-Z0-9 ]*', '', nline)
+#         if len(nline) > 0:
+#             clean_lines.append(nline+"\n\n")
+
+#     return clean_lines
+
+def fakecleanLines(lines):
     clean_lines = []
     for line in lines:
         if len(line.split()) < 5: continue
-
-        nline = re.sub('http[s]:\/\/\S+', '', line)
-        nline = re.sub('@\S+', '', nline)
-        nline = re.sub('\n', '', nline)
-        nline = nline.strip()
-        nline = nline.lower()
-        nline = re.sub('-', ' ', nline)
-        nline = re.sub('\'s', '', nline)
-        nline = re.sub('\'', '', nline)
-        nline = re.sub('\"', '', nline)
-        nline = re.sub('[^a-zA-Z0-9 ]*', '', nline)
-        if len(nline) > 0:
-            clean_lines.append(nline+"\n\n")
-
+        if len(line) > 0:
+            clean_lines.append(line+"\n\n")
     return clean_lines
 
-x = cleanLines(x)
+x = fakecleanLines(x)
 with open('shivu_majdoori.txt', 'w+') as f:
     f.writelines(x[:300])
 
