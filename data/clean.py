2709d2f22a87e28d3105ef05eeaf15767e724474
chaudharytanishq <tanishqchaudhary511@gmail.com>
2021-04-20 20:50:53 +0530

    Adding cleaning file

---
+++ data/clean.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import re
+
+df = pd.read_csv('parallel.csv')
+x = df['Sentence']
+
+
+
+def cleanLines(lines):
+    clean_lines = []
+    for line in lines:
+        if len(line.split()) < 5: continue
+
+        nline = re.sub('http[s]:\/\/\S+', '', line)
+        nline = re.sub('@\S+', '', nline)
+        nline = re.sub('\n', '', nline)
+        nline = nline.strip()
+        nline = nline.lower()
+        nline = re.sub('-', ' ', nline)
+        nline = re.sub('\'s', '', nline)
+        nline = re.sub('\'', '', nline)
+        nline = re.sub('\"', '', nline)
+        nline = re.sub('[^a-zA-Z0-9 ]*', '', nline)
+        if len(nline) > 0:
+            clean_lines.append(nline+"\n\n")
+
+    return clean_lines
+
+x = cleanLines(x)
+with open('shivu_majdoori.txt', 'w+') as f:
+    f.writelines(x[:300])
+
+with open('tani_majdoori.txt', 'w+') as f:
+    f.writelines(x[300:600])
